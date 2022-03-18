7f0d4041a86cca358ffcc7ff65c4a959653d406b
Jali1 <jalisihle1@outlook.com>
2022-03-18 14:11:56 +0200

    retrieve all recent movies

--- NewMovieReleases.py
+++ NewMovieReleases.py
@@ -1,3 +1,19 @@
+from matplotlib.pyplot import title
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+url = 'https://123-movie.cc/123movies-free-online/'
+r = requests.get(url)
+soup = BeautifulSoup(r.content,'html.parser')
+
+movie_titles = []
+for title in soup.findAll('span', {'class': 'mli-info'}):
+    titles =title.find('h2').get_text()
+    movie_titles.append(titles)
+
+topmovies=pd.DataFrame({
+    'Movie_Title': movie_titles
+})
+
+print(topmovies)
